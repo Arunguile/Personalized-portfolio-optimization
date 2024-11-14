9e4a852e48c3bf75dde7b918f3d8282b4f308ac5
Arunguile <87377303+Arunguile@users.noreply.github.com>
2024-11-14 17:43:41 +0000

    commit

--- riskScoreCalculation.py
+++ riskScoreCalculation.py
@@ -2,12 +2,12 @@ import numpy as np
 import joblib
 
 # -- Loading the trained model --
-model = joblib.load("./rf_model_final.joblib")
+model = joblib.load("./finModel.joblib")
 
 def calculate_risk_score(inputs):
     expected_features = model.n_features_in_
-
     print(f"Number of features expected by the model: {expected_features}")
+    
     input_data = np.array([inputs])
     predicted_risk_score = model.predict(input_data)[0]
 
